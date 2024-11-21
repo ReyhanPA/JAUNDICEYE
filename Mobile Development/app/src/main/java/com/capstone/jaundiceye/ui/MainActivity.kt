d36f1b707f32f0abb92bbe13a65a5afdf57dc3e5
ReyhanPA <reyhanputra95@gmail.com>
2024-11-21 16:57:08 +0700

    feat: slicing bottom navigation, toolbar, and fragment

--- Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/MainActivity.kt
+++ Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/MainActivity.kt
@@ -3,8 +3,6 @@ package com.capstone.jaundiceye.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.capstone.jaundiceye.R
 import com.capstone.jaundiceye.databinding.ActivityMainBinding
@@ -21,16 +19,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_recommendation, R.id.navigation_article
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.navigation_home -> {
+                    binding.textSubheader.text = getString(R.string.home_subheader_text)
+                    binding.toolbarTitle.text = getString(R.string.home_toolbar_text)
+                }
+                R.id.navigation_recommendation -> {
+                    binding.textSubheader.text = getString(R.string.recommendation_subheader_text)
+                    binding.toolbarTitle.text = getString(R.string.recommendation_toolbar_text)
+                }
+                R.id.navigation_article -> {
+                    binding.textSubheader.text = getString(R.string.article_subheader_text)
+                    binding.toolbarTitle.text = getString(R.string.article_toolbar_text)
+                }
+                R.id.navigation_profile -> {
+                    binding.textSubheader.text = getString(R.string.profile_subheader_text)
+                    binding.toolbarTitle.text = getString(R.string.profile_toolbar_text)
+                }
+            }
+        }
     }
 }

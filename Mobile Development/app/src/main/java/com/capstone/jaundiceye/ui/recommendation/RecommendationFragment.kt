98b1595fe69e5eabcdcfa9c1f88b6bf3c33c062c
ReyhanPA <reyhanputra95@gmail.com>
2024-11-25 15:07:12 +0700

    feat: slicing fragment recommendation

--- Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/recommendation/RecommendationFragment.kt
+++ Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/recommendation/RecommendationFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.capstone.jaundiceye.databinding.FragmentRecommendationBinding
@@ -26,11 +25,6 @@ class RecommendationFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val recommendationViewModel = ViewModelProvider(this)[RecommendationViewModel::class.java]
-
-        val textView: TextView? = binding?.textRecommendation
-        recommendationViewModel.text.observe(viewLifecycleOwner) {
-            textView?.text = it
-        }
     }
 
     override fun onDestroyView() {

d4accf4470b12abf97fd5e4137c5e99324b981da
ReyhanPA <reyhanputra95@gmail.com>
2024-12-03 03:28:40 +0700

    feat: update recommendation, article, and scanner

--- Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/scanner/ScannerFragment.kt
+++ Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/scanner/ScannerFragment.kt
@@ -1,4 +1,4 @@
-package com.capstone.jaundiceye.ui.article
+package com.capstone.jaundiceye.ui.scanner
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,25 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.capstone.jaundiceye.databinding.FragmentArticleBinding
+import com.capstone.jaundiceye.databinding.FragmentScannerBinding
 
-class ArticleFragment : Fragment() {
+class ScannerFragment : Fragment() {
 
-    private var _binding: FragmentArticleBinding? = null
+    private var _binding: FragmentScannerBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
-        _binding = FragmentArticleBinding.inflate(inflater, container, false)
+        _binding = FragmentScannerBinding.inflate(inflater, container, false)
         return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val articleViewModel = ViewModelProvider(this)[ArticleViewModel::class.java]
+        val scannerViewModel = ViewModelProvider(this)[ScannerViewModel::class.java]
     }
 
     override fun onDestroyView() {

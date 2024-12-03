139178245eb0739c351d2aca22e81403ced626c9
ReyhanPA <reyhanputra95@gmail.com>
2024-12-03 20:09:35 +0700

    feat: add camera and gallery

--- Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/scanner/ScannerActivity.kt
+++ Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/scanner/ScannerActivity.kt
@@ -1,13 +1,23 @@
 package com.capstone.jaundiceye.ui.scanner
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.capstone.jaundiceye.R
 import com.capstone.jaundiceye.databinding.ActivityScannerBinding
+import com.capstone.jaundiceye.util.getImageUri
 
 class ScannerActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityScannerBinding
+    private var currentImageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +25,71 @@ class ScannerActivity : AppCompatActivity() {
         binding = ActivityScannerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (!allPermissionsGranted()) {
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+
         binding.subheaderText.text = getString(R.string.subheader_scanner_text)
         binding.toolbarTitle.text = getString(R.string.header_scanner_text)
+        binding.apply {
+            buttonScannerGallery.setOnClickListener { startGallery() }
+            buttonScannerCamera.setOnClickListener { startCamera() }
+//            buttonScannerSave.setOnClickListener { save() }
+            toolbarBack.setOnClickListener { finish() }
+        }
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Permission request granted", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Permission request denied", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private fun allPermissionsGranted() =
+        ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { imageUri: Uri? ->
+        if (imageUri != null) {
+            currentImageUri = imageUri
+            showImage()
+        } else {
+            Log.d(TAG, "Error: ${resources.getString(R.string.error_no_image_selected)}")
+        }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(
+            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+        )
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { isSuccess ->
+        if (isSuccess) {
+            showImage()
+        } else {
+            currentImageUri = null
+        }
+    }
+
+    private fun startCamera() {
+        currentImageUri = getImageUri(this)
+        launcherIntentCamera.launch(currentImageUri!!)
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            binding.imgPreview.setImageURI(it)
+        }
+    }
+
+    companion object {
+        private const val TAG = "ScannerActivity"
     }
 }

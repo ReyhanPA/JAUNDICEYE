c56d12e48390d9c04d2322e87659bd56771c3961
ReyhanPA <reyhanputra95@gmail.com>
2024-12-05 01:30:53 +0700

    feat: add integration for profile

--- Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/profile/ProfileViewModel.kt
+++ Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/profile/ProfileViewModel.kt
@@ -3,11 +3,27 @@ package com.capstone.jaundiceye.ui.profile
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.capstone.jaundiceye.data.pref.UserModel
+import com.capstone.jaundiceye.repositories.UserRepository
+import com.capstone.jaundiceye.util.Event
+import kotlinx.coroutines.launch
 
-class ProfileViewModel : ViewModel() {
+class ProfileViewModel(private val repository: UserRepository) : ViewModel() {
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is profile Fragment"
+    private val _errorMessage = MutableLiveData<Event<String>>()
+    val errorMessage: LiveData<Event<String>> = _errorMessage
+
+    fun getSession(): LiveData<UserModel> {
+        return repository.getSession().asLiveData()
     }
-    val text: LiveData<String> = _text
+
+    fun logout() {
+        viewModelScope.launch {
+            repository.logout()
+        }
+    }
 }

189ffdbbbceec3419f704da9d7ca9b0ce9d2220b
arifu11 <arifuddinachmad11@gmail.com>
2024-12-06 17:25:26 +0700

    Perbaikan tampilan login, profile, dan register

--- Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/authentication/LoginActivity.kt
+++ Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/authentication/LoginActivity.kt
@@ -1,6 +1,14 @@
 package com.capstone.jaundiceye.ui.authentication
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +25,36 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+    //KODE BUAT SIGN UP NYA BIAR JADI BUTTON
+        val signUpText = findViewById<TextView>(R.id.tvSignUp)
+
+        // Kalimat lengkap
+        val text = "Belum punya akun? Sign Up"
+
+        // Membuat SpannableString
+        val spannableString = SpannableString(text)
+
+        // Menentukan klik pada bagian "Sign Up"
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                // Intent ke halaman Register
+                val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+                startActivity(intent)
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                // Menjaga agar underline tidak muncul
+                ds.isUnderlineText = false
+            }
+        }
+
+        // Memberi span pada kata "Sign Up" (posisi mulai dan akhir teks)
+        spannableString.setSpan(clickableSpan, 17, text.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        // Menetapkan teks dan mengaktifkan LinkMovementMethod
+        signUpText.text = spannableString
+        signUpText.movementMethod = LinkMovementMethod.getInstance()
     }
 }

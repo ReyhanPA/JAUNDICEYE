701b0135a715c2c132d499f3f63f8d5200b37c39
ReyhanPA <reyhanputra95@gmail.com>
2024-12-06 22:31:27 +0700

    feat: add integration for prfile fragment

--- Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/profile/ProfileViewModel.kt
+++ Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/profile/ProfileViewModel.kt
@@ -1,22 +1,14 @@
 package com.capstone.jaundiceye.ui.profile
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.capstone.jaundiceye.data.pref.UserModel
 import com.capstone.jaundiceye.repositories.UserRepository
-import com.capstone.jaundiceye.util.Event
 import kotlinx.coroutines.launch
 
 class ProfileViewModel(private val repository: UserRepository) : ViewModel() {
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading: LiveData<Boolean> = _isLoading
-
-    private val _errorMessage = MutableLiveData<Event<String>>()
-    val errorMessage: LiveData<Event<String>> = _errorMessage
-
     fun getSession(): LiveData<UserModel> {
         return repository.getSession().asLiveData()
     }

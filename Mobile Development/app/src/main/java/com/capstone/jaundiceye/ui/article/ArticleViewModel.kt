bf684b8750e4fef5c08abf2faa6d77bab22de0bb
ReyhanPA <reyhanputra95@gmail.com>
2024-12-06 22:29:43 +0700

    feat: add integration for article fragment

--- Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/article/ArticleViewModel.kt
+++ Mobile Development/app/src/main/java/com/capstone/jaundiceye/ui/article/ArticleViewModel.kt
@@ -3,11 +3,39 @@ package com.capstone.jaundiceye.ui.article
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.capstone.jaundiceye.data.pref.UserModel
+import com.capstone.jaundiceye.data.remote.responses.ArticlesResponseItem
+import com.capstone.jaundiceye.repositories.ArticlesRepository
+import com.capstone.jaundiceye.util.Event
+import kotlinx.coroutines.launch
 
-class ArticleViewModel : ViewModel() {
+class ArticleViewModel(private val repository: ArticlesRepository) : ViewModel() {
+    private val _articles = MutableLiveData<List<ArticlesResponseItem>>()
+    val articles: LiveData<List<ArticlesResponseItem>> = _articles
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is article Fragment"
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _errorMessage = MutableLiveData<Event<String>>()
+    val errorMessage: LiveData<Event<String>> = _errorMessage
+
+    fun getSession(): LiveData<UserModel> {
+        return repository.getSession().asLiveData()
     }
-    val text: LiveData<String> = _text
+
+    fun getArticles() {
+        _isLoading.value = true
+        viewModelScope.launch {
+            try {
+                val response = repository.getArticles()
+                _articles.postValue(response)
+            } catch (e: Exception) {
+                _errorMessage.postValue(Event("Error: ${e.message ?: "Unknown error"}"))
+            } finally {
+                _isLoading.value = false
+            }
+        }
+    }
 }
